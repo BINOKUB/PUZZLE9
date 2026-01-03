77cca700f73ca56b0cfb0208b095ffb50435cb58
BINOKUB <binokub@gmail.com>
2026-01-03 18:55:18 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - Service Worker Minimaliste
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
